a710abf66082a9bcba040e617ba04338e61f6348
Skevergreen <116793473+Skevergreen@users.noreply.github.com>
2023-03-23 20:54:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "7184257"))
 API_HASH = environ.get("API_HASH", "00db684ac4ab37587f5395581ae3a9c8")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5398153171:AAGz21acZSwOs-rLoSKy2n4RG0-Ii_jFB5I")
 ADMIN = int(environ.get("ADMIN", "1412758888"))          
 CAPTION = environ.get("CAPTION", "@oxyver")
 
